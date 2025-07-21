a0f5f3315ff8e00fa2dfe1ea13fd0262676a360c
k4d <n.siskevich@gmail.com>
2025-07-21 18:11:57 +0300

    Update project configuration
    - Update `package.json` with latest dependencies and scripts - Update
    `eslint.config.js` with latest rules and configurations - Set up ESLint
    with TypeScript and Astro support, including custom rules - Configure
    Prettier with custom settings and overrides

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,23 @@
-import tseslint from 'typescript-eslint';
-import astro from 'eslint-plugin-astro';
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
 
 export default tseslint.config(
+  // Base recommended rules for TypeScript
   ...tseslint.configs.recommended,
-  ...astro.configs['flat/recommended'],
+
+  // Recommended rules for Astro
+  ...astro.configs["flat/recommended"],
+
+  // I
   {
-    ignores: ['dist/', '.astro/'],
+    ignores: ["dist/", ".astro/"],
   },
+
+  // Override rules
   {
     rules: {
       // You can add any rule overrides here
-      '@typescript-eslint/no-unused-vars': 'warn',
+      "@typescript-eslint/no-unused-vars": "warn",
     },
-  }
+  },
 );
